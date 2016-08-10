ba1aa950f869903032dd887f0c6bf843368e6999
Lucy Knight <lucy.knight@gmail.com>
2016-08-10 13:45:01 +0100

    more hacking of mtg_parser.py

--- mtg_parser_2.py
+++ mtg_parser_2.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import html5lib
-with urllib.request.urlopen("http://democracy.devon.gov.uk/mgWebService.asmx/GetMeeting?lMeetingId=225") as response:
+with urllib.request.urlopen("http://democracy.devon.gov.uk/mgWebService.asmx/GetMeeting?lMeetingId=243") as response:
    html = response.read()
-soup = BeautifulSoup(html, 'html5lib')
+soup = BeautifulSoup(html, 'html.parser')
 #soup = BeautifulSoup(markup, "xml")
 #print (soup)
 #pretty = soup.b.prettify()
@@ -16,12 +15,12 @@ soup = BeautifulSoup(html, 'html5lib')
 #print (soup.find_all(["b"]))
 #section = soup.body.section
 #print (section)
-#print (headsup)
-all_b = soup.find_all("agendaitemtitle")
+#print (soup)
+all_b = soup.find_all("minutesitemnumber")
 #print (all_b)
 #stripped = soup.stripped_strings
 for foo in all_b:
-    for desc in foo.children:
+    for desc in foo.descendants:
         print (foo, " ", desc, "\n")
 #for string in stripped:
 #    print(repr(string), "\n")
